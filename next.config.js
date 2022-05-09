93b829fd05e16334986cf2828bfdf9e1476af297
Vlad Sushko <sushko.v.i@yandex.ru>
2022-05-09 13:04:01 +0300

    add pwa

--- next.config.js
+++ next.config.js
@@ -6,3 +6,13 @@ module.exports = {
 		APP_ENV: process.env.REACT_APP_ENV,
 	},
 };
+
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+	pwa: {
+		dest: 'public',
+		register: true,
+		skipWaiting: true,
+	},
+});
